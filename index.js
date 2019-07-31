9bae598d3ed9cfa38c7f5b4fbc7f151bd2ce92dd
Kaushik Ravikumar <kaushik.ravikumar@buzzfeed.com>
2019-07-31 12:23:28 -0400

    set up bot listening to tags functionality

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const SlackBot = require('slackbots');
-const axios = require('axios');
+// const auth_token = process.env.SLACK_AUTH_TOKEN;
 
 const bot = new SlackBot({
-    token: process.env.SLACK_AUTH_TOKEN,
+    token: "",
     name: 'corgi'
 });
 
 bot.on('message', (data) => {
     if(data.type === 'message' && data.files != null && data.channel != "")
     {
-        // bot.postMessage(data.channel, "Now, what tag(s) would you like to save this image under? Seperate with commas!");
-        // sendImageURL(data.files[0].url_private);
-        bot.postMessage(data.channel, data.files[0].url_private);
+        bot.postMessage(data.channel, "Now, what tag(s) would you like to save this image under? Seperate with commas!");
+        sendImageURL(data.files[0].url_private);
     }
 });
 
-// sendImageURL = (imageURL) => {
-//     bot.on('message', (data) => {
-//         if(data.type === 'message' && data.text != "")
-//         {
-//             var tags = data.text.split(",");
-//             tags.map(x => x.trim());
-//             storeImage(imageURL, tags);
-//         }
-//     });
-// };
+sendImageURL = (imageURL) => {
+    bot.on('message', (data) => {
+        if(data.type === 'message' && data.text != "")
+        {
+            var tags = data.text.split(",").map(x => x.trim());
+            storeImage(imageURL, tags);
+        }
+    });
+};
 
-// storeImage = (imageURL, tags) => {
-//     console.log(imageURL);
-//     for(var i = 0; i < tags.length; i++)
-//     {
-//         console.log(tags[i]);
-//     }
-// };
+function postData(url = '', data = {}) {
+    // Default options are marked with *
+      return fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, cors, *same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+              'Content-Type': 'application/json',
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrer: 'no-referrer', // no-referrer, *client
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+      })
+      .then(response => response.json()); // parses JSON response into native JavaScript objects 
+  }
+
+storeImage = (imageURL, tags) => {
+    // TODO MAKE POST REQUEST TO BACKEND SERVICE HERE!
+};
