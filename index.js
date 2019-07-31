d1767c538d6c247bd17ad21e997131745c33cbaa
Kaushik Ravikumar <kaushik.ravikumar@buzzfeed.com>
2019-07-31 11:24:33 -0400

    added token as secret

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new SlackBot({
-    token: 'xoxb-710293854900-712916556630-6K6Tl62u3oNDUecT1zbVzm7w',
+    token: process.env.SLACK_AUTH_TOKEN,
     name: 'corgi'
 });
 
 bot.on('message', (data) => {
-    if(data.type === 'message' && data.files != null)
+    if(data.type === 'message' && data.files != null && data.channel != "")
     {
+        // bot.postMessage(data.channel, "Now, what tag(s) would you like to save this image under? Seperate with commas!");
+        // sendImageURL(data.files[0].url_private);
         bot.postMessage(data.channel, data.files[0].url_private);
     }
 });
+
+// sendImageURL = (imageURL) => {
+//     bot.on('message', (data) => {
+//         if(data.type === 'message' && data.text != "")
+//         {
+//             var tags = data.text.split(",");
+//             tags.map(x => x.trim());
+//             storeImage(imageURL, tags);
+//         }
+//     });
+// };
+
+// storeImage = (imageURL, tags) => {
+//     console.log(imageURL);
+//     for(var i = 0; i < tags.length; i++)
+//     {
+//         console.log(tags[i]);
+//     }
+// };
